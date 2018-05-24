47a460aa561defe3ee9c8372d107bb9e9f4f86ff
yamadayasuhide <yamada@world-amenity.co.jp>
2018-05-24 14:29:16 +0900

    no message

---
+++ src/main/java/org/hellows/Hellows.java
@@ -0,0 +1,14 @@
+package org.hellows;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Hellows
+ */
+public class Hellows{
+	Logger logger = LoggerFactory.getLogger(this.getClass());
+	public void exec() {
+		logger.debug("Hellows exec");
+	}
+}
